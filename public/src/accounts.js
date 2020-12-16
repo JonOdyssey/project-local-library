21741f7a9567bc99feb3c7bc1ea360e15cfd6cd4
Jonathan Hernandez <72829148+JonOdyssey@users.noreply.github.com>
2020-12-16 14:58:29 -0800

    Feeling Good about this first draft

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,54 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  const accountsById = accounts.reduce((accu, account) => {
+    accu[account.id] = account;
+    return accu;
+  }, {});
+  return !accountsById[id] ? "ID not found" : accountsById[id];
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((accountA, accountB) => {
+    const textA = accountA.name.last.toLowerCase();
+    const textB = accountB.name.last.toLowerCase();
+    return textA > textB ? 1 : -1;
+  });
+}
 
-function numberOfBorrows(account, books) {}
+function numberOfBorrows(account, books) {
+  const accountId = account.id;
+  const borrowedBook = [];
+  const findBooks = books.forEach((book) => {
+    const borrowedHistory = book.borrows;
+    const filteredBooks = borrowedHistory.filter((borrowId) => {
+      if (borrowId.id == accountId) {
+        borrowedBook.push(book);
+      }
+    });
+  });
+  let count = borrowedBook.length;
+  return count;
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  const possessedBooks = [];
+  const accountId = account.id;
+  for (let index in books) {
+    const book = books[index];
+    const { id, title, authorId, genre, borrows } = book;
+    for (let jndex in borrows) {
+      if (borrows[jndex].id === accountId && borrows[jndex].returned === false) {
+        for (let kndex in authors) {
+          const author = authors[kndex];
+          if (author.id === book.authorId) {
+            let tempObj = { id, title, genre, authorId, author, borrows };
+            possessedBooks.push(tempObj);
+          }
+        }
+      }
+    }
+  }
+  return possessedBooks;
+}
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,44 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  const authorsById = authors.reduce((accu, author) => {
+    accu[author.id] = author;
+    return accu;
+  }, {});
 
-function findBookById(books, id) {}
+  return authorsById[id];
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function findBookById(books, id) {
+  const booksById = books.reduce((accu, book) => {
+    accu[book.id] = book;
+    return accu;
+  }, {});
+  return booksById[id];
+}
 
-function getBorrowersForBook(book, accounts) {}
+function partitionBooksByBorrowedStatus(books) {
+  const borBooks = books.filter((book) => book.borrows[0].returned === false);
+  const retBooks = books.filter((book) => book.borrows[0].returned === true);
+  return [borBooks, retBooks];
+}
+
+function getBorrowersForBook(book, accounts) {
+  const borrowArr = [];
+  const borrowLog = book.borrows;
+  const accountIds = accounts.reduce((accu, account) => {
+    accu[account.id] = account;
+    return accu;
+  }, {});
+  for (let index in borrowLog){
+    for (let jndex in accountIds) {
+      if (accountIds[jndex].id === borrowLog[index].id) {
+        const { returned } = borrowLog[index];
+        accountIds[jndex]["returned"] = returned;
+        borrowArr.push(accountIds[jndex]);
+      }
+    }
+  }
+  return borrowArr.slice(0, 10);
+}
 
 module.exports = {
   findAuthorById,
